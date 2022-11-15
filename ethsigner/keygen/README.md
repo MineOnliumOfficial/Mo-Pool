6bfbd5bebd06bb6e91839b1aa76c2a414929b9ff
Jeff <60796676+JeffNeff@users.noreply.github.com>
2022-11-14 16:40:52 -0800

    mo smal chainges

---
+++ ethsigner/keygen/README.md
@@ -0,0 +1,9 @@
+## Keygen
+
+This script exists to make it easy to create a new keyfile for ethsigner. 
+
+## usage
+```
+go mod tidy
+go run .
+```
---
+++ ethsigner/keygen/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/ethereum/go-ethereum/accounts/keystore"
+)
+
+func main() {
+	fmt.Println("Enter the password to encrypt the account")
+	var password string
+	fmt.Scanln(&password)
+	ks := keystore.NewKeyStore("./wallets", keystore.StandardScryptN, keystore.StandardScryptP)
+	account, err := ks.NewAccount(password)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// write the password to a 'passwords.txt' file
+	// this is not a secure way to store passwords
+	f := "passwords.txt"
+	file, err := os.OpenFile(f, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	if _, err := file.WriteString(password + "\n"); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(account.Address.Hex())
+}
